f737055c3c72fdfac672492d03fac3703e397fb9
Jeremy Ross <jeremysross@icloud.com>
2020-05-08 08:03:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+new Docute({
+  target: '#docute',
+  nav: [
+    {
+      title: 'Home',
+      link: '/'
+    },
+    {
+      title: 'About',
+      link: '/about'
+    }
+  ],
+  sidebar: [
+    {
+      title: 'Guide',
+      links: [
+        {
+          title: 'Introduction',
+          link: '/introduction'
+        },
+        {
+          title: 'Installation',
+          link: '/installation'
+        }
+      ]
+    }
+  ]
+})
